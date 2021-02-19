48fb139bb83af4f667d0d1fc5ccc7dd94cee91bd
Oscar Taracena <oscar.taracena@gmail.com>
2021-02-19 08:38:40 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,10 +5,14 @@ node default {
     owner   => 'root',
   }
 }
-  node 'master.puppet.vm' {
-    include role::master_server
+node 'master.puppet.vm' {
+  include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
   }
-
+}
 node /^web/ { 
   include role::app_server
 }
